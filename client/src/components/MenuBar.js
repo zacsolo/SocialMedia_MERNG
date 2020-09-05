ff4cd7b9f9883e4c3263a8ffb6a38757469756d5
Zac Solomon <zac@zacsolo.com>
2020-09-05 10:18:21 -0400

    Authenticated Routes for Logged In Users

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 
 import { Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
+import { AuthContext } from '../context/auth';
+
 export default function MenuExampleSecondaryPointing() {
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
   const path = pathname === '/' ? 'home' : pathname.substr(1);
   const [activeItem, setActiveItem] = useState(path);
@@ -12,7 +15,14 @@ export default function MenuExampleSecondaryPointing() {
     setActiveItem(name);
   };
 
-  return (
+  const menuBar = user ? (
+    <Menu pointing secondary size='massive' color='teal'>
+      <Menu.Item name={user.username} active as={Link} to='/' />
+      <Menu.Menu position='right'>
+        <Menu.Item name='logout' onClick={logout} />
+      </Menu.Menu>
+    </Menu>
+  ) : (
     <Menu pointing secondary size='massive' color='teal'>
       <Menu.Item
         name='home'
@@ -39,4 +49,6 @@ export default function MenuExampleSecondaryPointing() {
       </Menu.Menu>
     </Menu>
   );
+
+  return menuBar;
 }
--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,4 +1,17 @@
 import React, { useReducer, createContext } from 'react';
+import jwtDecode from 'jwt-decode';
+
+const initialState = { user: null };
+
+if (localStorage.getItem('jwtToken')) {
+  const decodedToken = jwtDecode(localStorage.getItem('jwtToken'));
+
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem('jwtToken');
+  } else {
+    initialState.user = decodedToken;
+  }
+}
 
 const AuthContext = createContext({
   user: null,
@@ -18,13 +31,15 @@ const authReducer = (state, action) => {
 };
 
 const AuthProvider = (props) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
   const login = (userData) => {
+    localStorage.setItem('jwtToken', userData.token);
     dispatch({ type: 'LOGIN', payload: userData });
   };
 
   const logout = () => {
+    localStorage.removeItem('jwtToken');
     dispatch({ type: 'LOGOUT' });
   };
 
